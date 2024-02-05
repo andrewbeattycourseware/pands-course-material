147530828c6b32e3571be54efb1c7b373d6c0908
Andrew Beatty <andrew.beatty@gmit.ie>
2024-02-05 16:55:04 +0000

    question from teams

--- code/topic03-variables-simple/q-from-teams.py
+++ code/topic03-variables-simple/q-from-teams.py
@@ -1,16 +1,17 @@
-# this is some code that I am reproducing from a question from teams
-# inputs should return a string
-# author: Andrew Beatty
+# This is some code that I am reproducing from a question from teams.
+# Inputs should return a string (and do for me)
+# Author: Andrew Beatty
 
-# this does not work because later I try to add a integer to number (number needs to be an int)
+# This input returns a string and causes the later line to throw an error (tyring to add an integer to a string)
 #number = input("please enter a number:")
 
-# this reads in the number and convert it to an int
+# This reads in the input and converts it to an integer
 number = int(input("please enter a number:"))
 
+# if the type of number is a string then this will throw an error
 newnumber = number + 1
 
-# old way of doing formatting not best practice
+# old way of doing formatting, not best practice
 print (str(number)+" plus one is "+ str(newnumber))
 
 # better way of doing formating
